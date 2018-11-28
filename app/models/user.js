ad909187f0f5aa78c8441d9e514316e840a15dd4
Ali Malik <amali28@uwo.ca>
2018-11-28 02:06:38 +0000

    Added route for creating a new user, new user model as well

---
+++ app/models/user.js
@@ -0,0 +1,11 @@
+var mongoose     = require('mongoose');
+var Schema       = mongoose.Schema;
+
+var UserSchema  = new Schema({
+	email: String,
+	password: String,
+	verificationCode: String,
+	isLoggedIn: Boolean
+});
+
+module.exports = mongoose.model('User', UserSchema);
--- server.js
+++ server.js
@@ -3,14 +3,19 @@
 // BASE SETUP
 // =============================================================================
 
-// call the packages we need
+// equivalent to imports
 var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
+var randomstring = require('randomstring');
 var mongoose   = require('mongoose');
 mongoose.connect('mongodb://amali28-se3316-amali28-lab5-6582532:27017/items', {useNewUrlParser: true}); // connect to our database
 
 var Item    = require('./app/models/item');
+
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 // configure app to use bodyParser()
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -39,6 +44,68 @@ router.get('/', function(req, res) {
 // more routes for our API will happen here
 
 //posting 
+
+router.route('/createuser')
+    .post(function(req,res){
+        
+        var email = req.body.email;
+        
+        // check if the email entered is a valid email
+        if (email == "" || !validator.isEmail(email)){
+            // string response 
+            return res.send({message: "Invalid email"});
+        }
+        
+        var password = req.body.password;
+        
+        if (password == ""){
+            return res.send({message: "Please enter a valid password"});
+        }
+        
+        // encryption of the password using bcrypt
+        var salt = bcrypt.genSaltSync(saltRounds);
+        var hash = bcrypt.hashSync(password, salt);
+        
+        // generating a random verification code for the user
+        var verificationCode = randomstring.generate();
+        
+        var newUser = new User ({
+           email: email,
+           password: hash,
+           verificationCode: verificationCode,
+           isLoggedIn: false
+        });
+        
+        User.find({'email':email}, function (err, acc){
+            // if the response received demonstrates that the account has not been created, go forward with signing up the user
+            if(acc[0] == null){
+                newUser.save(function(err){
+                    if (err){
+                        res.send(err);
+                    }
+                    res.json({message: "Account has successfully been created"});
+                });
+            } else {
+                res.json({message: "An account with this email already exists"});
+            }
+            if (err){
+                res.send(err);
+            }
+        });
+        
+        var emailVerification;
+        
+        emailVerification={
+            to : req.body.email,
+            subject : "PARKA EMAIL VERIFICATION",
+            html : "Use the following code to verify your account: " + newUser.verificationCode
+        }
+
+        
+    })
+
+
+
 
 router.route('/items')
 
